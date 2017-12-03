8a151a53d2e3798e9fab947467a9038ed62c670d
bluelovers <codelovers@users.sourceforge.net>
2017-12-03 09:39:48 +0800

    . (Step: 1)
    Step: 1
    
    [ M] modified - Changes not staged for commit:
    
    	index.ts

--- index.ts
+++ index.ts
@@ -1,31 +1,41 @@
 import * as I18next from 'i18next'
 
-export default function (Vue) {
+export default function (Vue)
+{
 	var i18n = I18next.use({
 		type: "backend",
 
-		init: function (services, options, i18nextOpts) {
+		init: function (services, options, i18nextOpts)
+		{
 			this.load = options.load
 			if (typeof this.load !== 'function')
+			{
 				throw "vue-i18next: missing 'load' function in backend options"
+			}
 		},
 
-		read: function (language, namespace, callback) {
-			if (language == 'dev') {
+		read: function (language, namespace, callback)
+		{
+			if (language == 'dev')
+			{
 				callback(null, null)
 				return
 			}
-			this.load(language, namespace).then(function (module) {
+			this.load(language, namespace).then(function (module)
+			{
 				callback(null, module[language])
-			}).catch(function (err) {
+			}).catch(function (err)
+			{
 				callback(err, null)
 			})
 		}
 	})
 
-	Vue.use(function (Vue) {
+	Vue.use(function (Vue)
+	{
 		var i18nVm = new Vue({ data: { tag: 1 } })
-		i18n.on('languageChanged loaded', function () {
+		i18n.on('languageChanged loaded', function ()
+		{
 			// trigger a reactive change
 			i18nVm.tag++
 		})
@@ -33,7 +43,8 @@ export default function (Vue) {
 		Object.defineProperty(i18n, '$_vm', { value: i18nVm })
 		Object.defineProperty(Vue.prototype, '$i18n', { value: i18n })
 		Object.defineProperty(Vue.prototype, '$t', {
-			value: function (key, options) {
+			value: function (key, options)
+			{
 				this.$i18n.$_vm.tag // this makes the function reactive
 				return this.$i18n.t(key, options)
 			}
