6ed8fab30598958bb9b68af8d23fc2f500c76682
bluelovers <codelovers@users.sourceforge.net>
2017-12-03 23:05:12 +0800

    . (Step: 3)
    Step: 3
    
    [A ] new file:
    
    	test/_local-dev.ts
    	test/i18next.test.ts
    	test/res/locales.ts
    
    [ M] modified - Changes not staged for commit:
    
    	index.ts
    	package.json

---
+++ test/res/locales.ts
@@ -0,0 +1,25 @@
+/**
+ * Created by user on 2017/12/3/003.
+ */
+
+export const locales = {
+	en: {
+		message: {
+			hello: 'Hello!! - EN',
+		},
+		tos: 'Term of Service',
+		term: 'I accept {{1}} {{0}}.',
+		loadbundle: 'Load Bundle {{lang}}',
+	},
+	de: {
+		message: {
+			hello: 'Hallo!! - DE',
+		},
+		tos: 'Nutzungsbedingungen',
+		term: 'Ich akzeptiere {{0}}. {{1}}',
+		loadbundle: 'Bundle Laden {{lang}}',
+	},
+};
+
+// @ts-ignore
+export default exports;
